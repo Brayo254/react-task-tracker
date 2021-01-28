6c011b54c1bab3ca552a94d727664f4e27a7e0cb
Brian Itumo <b.itumo18@gmail.com>
2021-01-28 18:34:33 +0300

    My first 2021 github app

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div>
+      <h4>V 1.023</h4>
+      <Link to="/">Go back</Link>
+    </div>
+  );
+};
+
+export default About;
